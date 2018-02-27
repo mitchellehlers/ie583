df66d0c68f0ac653ead570aaf7b490203a547849
Mitchell Ehlers <mitchellehlers@Mitchells-MacBook-Pro.local>
2018-02-26 21:05:07 -0600

    hw2: initial commit

---
+++ hw2/hw2-source.r
@@ -0,0 +1,3 @@
+#Data source = https://archive.ics.uci.edu/ml/datasets/congressional+voting+records
+voteData <- read.table("/Users/mitchellehlers/Documents/IowaState-MasterBA/ie583/hw2/house-votes-84.data",header=T,sep=",",quote="\"")
+
