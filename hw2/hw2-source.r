ae0c047efc9fd097d64e7fbaaf720a1b2ace54fc
Mitchell Ehlers <ehlersmitchelld@johndeere.com>
2018-03-01 18:24:25 -0600

    hw2: best assoication rules for any attribute

--- hw2/hw2-source.r
+++ hw2/hw2-source.r
@@ -3,13 +3,14 @@ library(caret)
 library(arules)  
 library(arulesViz)
 
-voteData <- read.table("/Users/mitchellehlers/bench/ie583/hw2/vote.csv",header=T,sep=",",quote="\"")
+voteData <- read.table("C:\\bench\\iowastate\\ie583\\hw2\\vote.csv",header=T,sep=",",quote="\"")
 voteData[] = lapply(voteData[],factor)
 
 rules = apriori(voteData)
 summary(rules)
 plot(rules)
 
+##################
 good_rules = rules[quality(rules)$confidence>0.9] 
 plot(good_rules)
 
@@ -20,20 +21,40 @@ good_rules = good_rules[quality(good_rules)$lift>1.8]
 plot(good_rules)
 
 plot(good_rules, method="graph", itemLabels=FALSE)
+##################
 
-#Best Association Rule
+######Best Association Rule######
 inspect(head(rules, n=10, by = "confidence"))
 inspect(head(rules, n=10, by = "support"))
 inspect(head(rules, n=10, by = "lift"))
 
 plot(head(rules, n=10, by = "lift"), method="graph", itemLabels=FALSE)
 
-general_rules = subset(rules, subset = lift > 2.0) 
-inspect(head(general_rules, n=10, by = "confidence"))
+general_rules = subset(rules, subset = lift > 2.0)
 inspect(head(general_rules, n=10, by = "support"))
-inspect(head(general_rules, n=10, by = "lift"))
+plot(head(general_rules, n=10, by = "support"))
+plot(head(general_rules, n=10, by = "support"), method="graph", itemLabels=FALSE)
 
-#Best Rules Class=democrat
+elsalvadoraid_rules = subset(rules, subset = rhs %in% "el_salvador_aid=y") 
+inspect(head(elsalvadoraid_rules, n=10, by = "support"))
+inspect(head(elsalvadoraid_rules, n=10, by = "confidence"))
+inspect(head(elsalvadoraid_rules, n=10, by = "lift"))
+
+#Independent Test Set
+set.seed(1234)
+trainIndex <- createDataPartition(voteData$el_salvador_aid,p=.67,list=FALSE,times=1)
+voteTrain <- voteData[trainIndex,]
+voteTest<-voteData[-trainIndex,]
+
+#Decision Tree
+DTmodel<-J48(el_salvador_aid ~ .,data=voteTrain)
+prediction<-predict(DTmodel,voteTest)
+confusionMatrix(prediction,voteTest$el_salvador_aid)
+plot(DTmodel)
+
+#####################################
+
+######Best Rules Class=democrat######
 Dem_rules = subset(rules, subset = rhs %in% "Class=democrat") 
 plot(Dem_rules)
 
@@ -41,7 +62,9 @@ inspect(head(Dem_rules, n=10, by = "confidence"))
 inspect(head(Dem_rules, n=10, by = "support"))
 inspect(head(Dem_rules, n=10, by = "lift"))
 
-#Best Rules Class=republican
+#####################################
+
+######Best Rules Class=republican######
 Rep_rules = subset(rules, subset = rhs %in% "Class=republican") 
 plot(Rep_rules)
 
@@ -53,17 +76,7 @@ plot(head(Rep_rules, n=10, by = "confidence"))
 plot(head(Rep_rules, n=10, by = "support"))
 plot(head(Rep_rules, n=10, by = "lift"))
 
-#Independent Test Set
-set.seed(1234)
-trainIndex <- createDataPartition(voteData$adoption_of_the_budget_resolution,p=.67,list=FALSE,times=1)
-voteTrain <- voteData[trainIndex,]
-voteTest<-voteData[-trainIndex,]
-
-#Decision Tree
-DTmodel<-J48(adoption_of_the_budget_resolution ~ .,data=voteTrain)
-prediction<-predict(DTmodel,voteTest)
-confusionMatrix(prediction,voteTest$adoption_of_the_budget_resolution)
-plot(DTmodel)
+###########################################
 
 ctrl<-trainControl(method="cv", number = 10, savePred=T,classProb=T)
 treeGrid <- expand.grid(C=(1:5)*0.01, M=5)
