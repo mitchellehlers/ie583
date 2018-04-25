e2815084aba36106fb459564201ca9e175c313b8
Mitchell Ehlers <ehlersmitchelld@johndeere.com>
2018-04-24 21:19:05 -0500

    final-project: added a dervied attribute, hour, as it slightly improve RF and DT performance

--- final-project/project-source.r
+++ final-project/project-source.r
@@ -90,7 +90,7 @@ projTrainFS<-train_sbset[, c(1,2,3,4,5,6,8,9,10,11,12,13,14,15,16,17,18,19,20)]
 head(projTrainFS)
 
 # Split off only the important attributes
-projTrainFS<-train_sbset[, c(1,2,3,4,5,8)]
+projTrainFS<-train_sbset[, c(1,2,3,4,5,8,9)]
 head(projTrainFS)
 
 ######################################
